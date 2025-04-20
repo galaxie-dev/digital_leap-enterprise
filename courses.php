dc76a6ee15b38765e31c5c1f7122e3d8cfb3af89
Evans Osumba <osumbaevanzz@gmail.com>
2025-04-20 06:26:06 +0300

    returned css

--- courses.php
+++ courses.php
@@ -156,7 +156,7 @@ function getCourseIdByName($conn, $name) {
 
 
     <!-- Courses Start -->
-    <div class="container-xxl py-5">
+    <!-- <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
                 <h6 class="section-title bg-white text-center text-primary px-3">Courses</h6>
@@ -250,7 +250,7 @@ function getCourseIdByName($conn, $name) {
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
     <!-- Courses End -->
 
 
