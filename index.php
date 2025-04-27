6b8f15ff2706ff7e0a44a37dfef98b9ed3ff6db7
Evans Osumba <osumbaevanzz@gmail.com>
2025-04-27 15:47:14 +0300

    notes

--- index.php
+++ index.php
@@ -279,7 +279,7 @@ session_start();
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="team-item bg-light">
                         <div class="overflow-hidden">
-                            <img class="img-fluid" src="images/evans-ceo.jpg" alt="">
+                            <img class="img-fluid" src="images/osumba.jpg" alt="">
                         </div>
                         <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
                             <div class="bg-light d-flex justify-content-center pt-2 px-1">
